8695bd8ebddda582a3d3ce4b4e2d41a2d9348b4f
CodysEyelids <80240545+CodysEyelids@users.noreply.github.com>
2021-03-08 01:35:16 -0500

    Create Blink.py

---
+++ Blink.py
@@ -0,0 +1,9 @@
+# pip install pyfiglet
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("HE BLINKED")
+print(ascii_banner)
+
+
+print ('cody will blink in his next video due to your')
+print ('waste of time running this script lol')
